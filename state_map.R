2854097b8b196879c913ffb8ce1d4b9a8599227e
Gabriel Eugenio <gabriell.eugenio@gmail.com>
2020-11-26 23:11:52 -0300

    Update state_map.R

--- state_map.R
+++ state_map.R
@@ -1,11 +1,15 @@
+# intalling libraries
 library(geobr)
 library(ggplot2)
 library(readxl)
 
+# select plot state in Brazil
 CE <- read_state(code_state = 'CE')
 
+# path's dataset of points
 df <- read_excel("C:/Users/GABRIEL/Desktop/TCC/GD1.xlsx")
 
+# generate map with data points
 ggplot(CE) + geom_sf()+
   geom_point(data = df ,
              aes(x = LON , y = LAT , size= POTENCIA),
